296bde9d57d6b525f479a778bb34af5d902e0088
Todd Stoneberg <todd.stoneberg@gmail.com>
2021-06-10 20:43:44 -0600

    Done.

--- src/App.js
+++ src/App.js
@@ -34,8 +34,22 @@ class App extends Component {
 }
 
 const mapStateToProps = (state) => {
-  debugger;
+  // debugger;
   return { items: state.items }
 }
 
+// const mapStateToProps = (state) => {
+//   return { orangePeel: ['a', 'b', 'c'] }
+// } 
+
 export default connect(mapStateToProps)(App);
+
+
+// const vanilla = (milkshake) => {
+//   debugger;
+//   return { items: milkshake.items }
+// }
+
+// export default connect(vanilla)(App); 
+
+// export default connect( state => ({ items: state.items }) )(App);
